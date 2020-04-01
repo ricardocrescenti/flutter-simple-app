b46debf6d70814eeea2638a8fd5c5d6a297a76c0
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2020-04-01 13:19:39 -0300

    Added `@mustCallSuper` on `signOut` in `AuthServicePattern`

--- lib/services/auth_service_pattern.dart
+++ lib/services/auth_service_pattern.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:module_provider/module_provider.dart';
 
 abstract class AuthServicePattern extends Service {
@@ -35,7 +36,8 @@ abstract class AuthServicePattern extends Service {
     return null;
   }
 
-  signOut() async {
+  @mustCallSuper
+  Future signOut() async {
     await auth.signOut();
   }
 
