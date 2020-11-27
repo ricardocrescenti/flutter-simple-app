adce51d44679388e40d98e074538d8a485061b27
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2020-11-27 15:36:20 -0300

    Adicionado StandardFormController

---
+++ lib/pages/standard_form/standard_form_controller.dart
@@ -0,0 +1,7 @@
+import 'package:simple_app/simple_app.dart';
+
+class StandardFormController<T> extends Controller {
+	T registry;
+  	
+	StandardFormController(Module module) : super(module);
+}
--- lib/pages/standard_form/standard_form_page.dart
+++ lib/pages/standard_form/standard_form_page.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:module_provider/module_provider.dart';
+import 'package:simple_app/pages/standard_form/standard_form_controller.dart';
 import 'package:simple_app/simple_app.dart';
 import 'package:simple_app/widgets/standard_form/standard_form.dart';
 import 'package:simple_form/simple_form.dart';
 import 'package:standard_dialogs/standard_dialogs.dart';
 
-abstract class StandardFormPage<T> extends Component {
+abstract class StandardFormPage<T> extends Component<StandardFormController<T>> {
+	
+	@override
+  	StandardFormController<T> initController(BuildContext context, Module module) => StandardFormController<T>(module);
 
 	@override
-	Widget build(BuildContext context, Controller controller) {
+	Widget build(BuildContext context, StandardFormController<T> controller) {
 		return StandardForm(
 			title: buildTitle(context, controller),
 			actions: buildActions(context, controller),
@@ -18,26 +22,26 @@ abstract class StandardFormPage<T> extends Component {
 		);
 	}
 
-	Widget buildTitle(BuildContext context, Controller controller);
+	Widget buildTitle(BuildContext context, StandardFormController<T> controller);
 
-	List<Widget> buildActions(BuildContext context, Controller controller) {
+	List<Widget> buildActions(BuildContext context, StandardFormController<T> controller) {
 		return [];
 	}
 
-	Widget buildForm(BuildContext context, Controller controller, ValuesProvider values, FormLayout formLayout);
+	Widget buildForm(BuildContext context, StandardFormController<T> controller, ValuesProvider values, FormLayout formLayout);
 
-	Map<String, dynamic> processInitialValues(BuildContext context, Controller controller) {
+	Map<String, dynamic> processInitialValues(BuildContext context, StandardFormController<T> controller) {
 		return initialValues(context, controller);
 	}
 
-	Map<String, dynamic> initialValues(BuildContext context, Controller controller);
+	Map<String, dynamic> initialValues(BuildContext context, StandardFormController<T> controller);
 
-	Future<T> processSave(BuildContext context, Controller controller, ValuesProvider values) async {
+	Future<T> processSave(BuildContext context, StandardFormController<T> controller, ValuesProvider values) async {
 		return await showAwaitDialog<T>(context, 
 			message: Text(SimpleAppLocalization.of(context)[SimpleAppLocalizationEnum.saving]), 
 			function: (context, updateMessage) => this.save(context, controller, values.getValues())
 		);
 	}
 
-	Future<T> save(BuildContext context, Controller controller, Map<String, dynamic> json);
+	Future<T> save(BuildContext context, StandardFormController<T> controller, Map<String, dynamic> json);
 }
