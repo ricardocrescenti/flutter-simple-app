8e4e26e07e1f3a3667eeb03ec8a1b3c03f94acf4
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2021-02-15 08:50:59 -0300

    Alterado ordem dos parâmetros

--- lib/widgets/standard_body/standard_body.dart
+++ lib/widgets/standard_body/standard_body.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 class StandardBody extends StatelessWidget {
 	static const EdgeInsets defaultPadding = const EdgeInsets.all(5);
 
-	final Widget child;
 	final EdgeInsets padding;
 	final bool scroll;
+	final Widget child;
 
 	StandardBody({
-		@required this.child,
 		this.padding = StandardBody.defaultPadding,
 		this.scroll = false,
+		@required this.child,
 	});
 
 	@override
